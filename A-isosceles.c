53161308695eaf5521058cf2efb11497af7eca1a
Ashwikha <ashwikhas@gmail.com>
2026-02-16 10:35:42 +0530

    Add files via upload

---
+++ A-isosceles.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  scanf("%d %d %d",&a,&b,&c);
+  if(a!=b && b!=c && a!=c){
+    printf("No");
+  }
+  else
+  {
+    printf("Yes");
+  }
+  return 0;
+}
